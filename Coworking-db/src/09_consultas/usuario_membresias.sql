010f5b1588255ef5d3bcdd01be79be85be4d387d
Luque-hlp <lujury.hack@gmail.com>
2026-07-26 21:35:39 -0500

    build(Create Archives): :memo:

--- Coworking-db/src/09_consultas/usuario_membresias.sql
+++ Coworking-db/src/09_consultas/usuario_membresias.sql
@@ -16,3 +16,10 @@ INNER JOIN membresias m ON m.id_usuario = u.id_usuario
 WHERE m.estado = 'Activa'
   AND CURDATE() BETWEEN m.fecha_inicio AND m.fecha_fin
 ORDER BY u.apellidos;
+
+-- 3. Listar los usuarios cuya membresia esta vencida.
+SELECT DISTINCT u.id_usuario, u.nombre, u.apellidos, m.fecha_fin
+FROM usuarios u
+INNER JOIN membresias m ON m.id_usuario = u.id_usuario
+WHERE m.estado = 'Vencida'
+ORDER BY m.fecha_fin DESC;
